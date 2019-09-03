b78abdce23eb4fe9717648eeb92fc73ee279a95f
Gavin <gd221717@ohio.edu>
2019-09-03 17:08:13 -0400

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,19 @@
+/*
+*   File: git-lab-program.cc
+*   Author: Gavin Dassatti
+*   Date: 09/03/19
+*   Description: 
+*/
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+int main(int argc, char const *argv[]){
+
+    cout << "Hello Git!!" << endl;
+
+
+    return 0;
+}
